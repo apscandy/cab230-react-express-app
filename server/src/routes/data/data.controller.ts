24e3daa79ffb745c664299c74b4d98d5b84f495e
Andrew Clarke <74689127+apscandy@users.noreply.github.com>
2024-05-12 12:30:40 +1000

    saving state before webpack fucks shit up

--- server/src/routes/data/data.controller.ts
+++ server/src/routes/data/data.controller.ts
@@ -1,22 +1,27 @@
 import { Request, Response } from "express";
-import { getCountriesFromDatabase } from "./data.services";
+import { getCountriesFromDatabase, getVolcanoesFromDatabase } from "./data.services";
 
-type populatedDistance = 'none' | '5km' | '10km' | '30km' | '100km';
-
-function checkValidPopulatedDistance(query: string){
-    return ['none', '5km', '10km', '30km', '100km'].includes(query);
+function isValidPopulatedDistance(query: string){
+    return ['5km', '10km', '30km', '100km'].includes(query);
 }
 
 export async function getCountries(req: Request, res: Response) {
     const data = await getCountriesFromDatabase()
     res.status(200).json(data);
-
+    return;
 }
 
 export async function getVolcanoes(req: Request, res: Response) {
-    const country = req.query.country;
+    const country = req.query.country as string;
     const populatedWithin: string = req.query.populatedWithin as string;
-    if (country === null) {
+    if (!country  && !populatedWithin) {
+        res.status(400).json({
+            error: true,
+            message: "Invalid query parameters. Only country and populatedWithin are permitted."
+        })
+        return;
+    }
+    if (!country) {
         res.status(400).json({
             error: true,
             message: "Country is a required query parameter."
@@ -24,19 +29,26 @@ export async function getVolcanoes(req: Request, res: Response) {
         return;
     }
     
-    if (populatedWithin !== null){
-        if(!checkValidPopulatedDistance(populatedWithin)){
+    if (populatedWithin){
+        if(!isValidPopulatedDistance(populatedWithin)){
             res.status(400).json({
                 error: true,
                 message: "Invalid value for populatedWithin. Only: 5km,10km,30km,100km are permitted."
             })
             return;
         }
+        const dist: number = parseInt(populatedWithin.split('km')[0])
+        const data = await getVolcanoesFromDatabase(country, dist);
+        res.status(200).json(data)
+        return;
     }
-    res.sendStatus(200);
+    const data = await getVolcanoesFromDatabase(country, null);
+    res.status(200).json(data)
+    return;
 }
 
 export async function getVolcanoByID(req: Request, res: Response) {
     const auth = res.locals.authenticated;
     res.sendStatus(200);
+    return;
 }
--- server/src/routes/data/data.services.ts
+++ server/src/routes/data/data.services.ts
@@ -1,4 +1,4 @@
-import  Knex  from "knex";
+import Knex from "knex";
 import knexConfig from "../../configurations/database";
 const database = Knex(knexConfig)
 
@@ -14,7 +14,7 @@ interface volcanoesInCountry {
     subregion: string
 }
 
-interface basicVolcanoesData extends volcanoesInCountry{
+interface basicVolcanoesData extends volcanoesInCountry {
     last_eruption: string
     summit: number
     elevation: number
@@ -22,50 +22,56 @@ interface basicVolcanoesData extends volcanoesInCountry{
     longitude: string
 }
 
-interface privilegedVolcanoesData extends basicVolcanoesData{
+interface privilegedVolcanoesData extends basicVolcanoesData {
     population_5km: number,
     population_10km: number,
     population_30km: number,
     population_100km: number
 }
 
- export async function getCountriesFromDatabase(): Promise<country[]> {
+export async function getCountriesFromDatabase(): Promise<country[]> {
     const data = await database("data").distinct("country").orderBy('country', 'asc');
     return data.map((item) => item.country);
-  }
-  
-  async function getVolcanoesFromDatabase(country: string): Promise<volcanoesInCountry[]> {
+}
+
+export async function getVolcanoesFromDatabase(country: string, distance: number | null): Promise<volcanoesInCountry[]> {
+    if (distance !== null) {
+        return await database("data")
+            .where({ country: country }).andWhere(`population_${distance}km`, '>=', '1')
+            .select("id", "name", "country", "region", "subregion");
+    }
+    return await database("data")
+        .where({ country: country })
+        .select("id", "name", "country", "region", "subregion");
+
+}
+
+async function getVolcanoByIDFromDatabase(id: number): Promise<basicVolcanoesData[]> {
     const data = await database("data")
-      .where({ country: country })
-      .select("id", "name", "country", "region", "subregion");
+        .where({ id: id })
+        .select(
+            "id",
+            "name",
+            "country",
+            "region",
+            "subregion",
+            "last_eruption",
+            "summit",
+            "elevation",
+            "latitude",
+            "longitude"
+        );
     return data;
-  }
-  
-  async function getVolcanoByIDFromDatabase(id: number): Promise<basicVolcanoesData[]> {
-    const data = await database("data")
-      .where({ id: id })
-      .select(
-        "id",
-        "name",
-        "country",
-        "region",
-        "subregion",
-        "last_eruption",
-        "summit",
-        "elevation",
-        "latitude",
-        "longitude"
-      );
-    return data;
-  }
-  
-  async function getVolcanoByIDFromDatabaseProtected(id: number): Promise<privilegedVolcanoesData[]> {
-      const data = database("data")
+}
+
+async function getVolcanoByIDFromDatabaseProtected(id: number): Promise<privilegedVolcanoesData[]> {
+    const data = database("data")
         .where({ id: id })
         .select();
-      return data;
-    }
-  
+    return data;
+}
+
 module.exports = {
-    getCountriesFromDatabase: getCountriesFromDatabase
+    getCountriesFromDatabase: getCountriesFromDatabase,
+    getVolcanoesFromDatabase: getVolcanoesFromDatabase
 }
