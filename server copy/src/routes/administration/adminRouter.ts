521f184501c5e458b267f67ad53e165ca7321cbd
Andrew Clarke <74689127+apscandy@users.noreply.github.com>
2024-06-01 23:46:52 +1000

    done

--- server copy/src/routes/administration/adminRouter.ts
+++
@@ -1,6 +0,0 @@
-import { Express } from "express"
-import { getMe } from "./me/getMe.controller"
-
-export default function (app: Express) {
-    app.get("/me", getMe)
-}
--- server copy/src/routes/authentication/authRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Express } from "express";
-import { login } from "./postLogin/postLogin.controller";
-import { register } from "./postRegister/postRegister.controller";
-
-export default function(app: Express) {
-    app.post("/user/login", login)
-    app.post("/user/register", register)
-}
